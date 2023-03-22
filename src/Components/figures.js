5460fe668908c10b9354ae495ba144a395011b12
Alina-Nasir <anasir.bese20seecs@seecs.edu.pk>
2023-03-22 23:51:11 +0500

    Changes

--- src/Components/figures.js
+++ src/Components/figures.js
@@ -74,7 +74,7 @@ function Figures() {
           </button>
           <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true" />
-            <span className="visually-hidden">Back</span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
       </div>
